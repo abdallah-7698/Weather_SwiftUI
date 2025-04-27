42dd99bb4d1417f2ceb4cf4058e401cc6c0c2b65
Abdallah Mohammed <abdallahabdulzahir@gmail.com>
2025-04-27 15:51:53 +0300

    Initial Commit

---
+++ Weather_SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather_SwiftUI
+//
+//  Created by name on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
